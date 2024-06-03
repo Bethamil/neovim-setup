bb3583c9dc22e0a5a3714eb8c5980cda0af662d6
Emiel <emiel92@gmail.com>
2024-06-03 19:31:02 +0200

    Initial commit

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+      },
+    },
+  },
+  config = function()
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+  end,
+}
