f4dad0617de240fa4614425b986e70828ddedf8e
Emiel Bloem <e.bloem@dictu.nl>
2024-06-05 16:15:37 +0200

    added follow_current_file to neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -20,6 +20,10 @@ return {
         filtered_items = {
           hide_dotfiles = false,
         },
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = false,
+        },
       },
     })
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
