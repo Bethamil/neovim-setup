d7de4d98f913d62c4abb77fecef762ab3095049a
Emiel <92210989+Bethamil@users.noreply.github.com>
2024-06-07 11:29:44 +0200

    add basic which-key setup

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,10 @@
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	config = function()
+		local wk = require("which-key")
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+		wk.setup({})
+	end,
+}
